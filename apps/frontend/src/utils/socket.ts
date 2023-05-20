a3eea5e25302e5a086b6b9374a020f4c14a429c9
Ray <62081739+SR0725@users.noreply.github.com>
2023-05-21 07:42:18 +0800

    feat: 修改後端連接方式

--- apps/frontend/src/utils/socket.ts
+++ apps/frontend/src/utils/socket.ts
@@ -5,6 +5,6 @@ const URL =
   process.env.NEXT_PUBLIC_ENV === "DEV" ||
   process.env.NEXT_PUBLIC_ENV === "LOCAL"
     ? `http://localhost:${process.env.NEXT_PUBLIC_BACKEND_PORT}`
-    : `https://ray-realms.com:${process.env.NEXT_PUBLIC_BACKEND_PORT}`;
+    : `https://socketio.ray-realms.com`;
 
 export const socket = io(URL);
